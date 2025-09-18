3a5266abcdee6c68b0cd68937aa3dfc34d96f693
Kayden Tarvaran <ktarvaran0@saddleback.edu>
2025-09-18 16:14:55 -0700

    G2: menus + stubs + cashier slip

--- invmenu.cpp
+++ invmenu.cpp
@@ -1,1 +1,23 @@
+/*********************************************************************
+* CS1B – G2: Serendipity
+*  Partner A: Sean Hanabusa (ID) — role: Cashier.cpp
+*  Partner B: Kayden Tarvaran (ID) — role: invmenu.cpp, main.cpp, reports.cpp
+*  Date: 2025‑09‑18
+*  Purpose: Menu navigation and validation practice: loop Main/Inventory/Reports, call function stubs, and print a two-decimal cashier test receipt
+*  Build:   g++ -std=c++20 mainmenu.cpp cashier.cpp invmenu.cpp reports.cpp -o serendipity
+***********************************************************************/
 
+
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+#include <vector>
+#include <thread> //needed for pauseSeconds(x);
+#include <chrono>
+using namespace std;
+
+
+void invMenu(){
+
+    
+}
