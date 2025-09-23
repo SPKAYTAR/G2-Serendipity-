355fab37380c5efc0448460ad931ae2e73c724e6
Kayden Tarvaran <ktarvaran0@saddleback.edu>
2025-09-22 20:42:49 -0700

    added report file and updated invmenu with header :wg

--- reports.cpp
+++ reports.cpp
@@ -12,6 +12,119 @@
 #include <iomanip>
 #include <cctype>
 #include <vector>
-#include <thread> //needed for pauseSeconds(x);
-#include <chrono>
+#include <limits>
 using namespace std;
+#include "reports.h";
+
+
+
+void reports(){
+    char c;
+    do{
+        cout << "\x1B[2J\x1B[H";
+        cout << "==============================\n";
+        cout << "Serendipity Booksellers\n";
+        cout << "Reports\n";
+        cout << "------------------------------\n";
+        cout << "1. Inventory Listing\n";
+        cout << "2. Inventory Wholesale Value\n";
+        cout << "3. Inventory Retail Value\n";
+        cout << "4. Listing by Quantity\n";
+        cout << "5. Listing by Cost\n";
+        cout << "6. Listing by Age\n";
+        cout << "7. Return to the Main Menu\n";
+        cout << "------------------------------\n";
+        cout << "Enter choice: ";
+        
+        cin >> c;
+
+        switch(c){
+            case '1': {
+                repListing();
+                break;
+            }
+
+            case '2': {
+                repWholesale();
+                break;
+            }
+
+            case '3': {
+                repRetail();
+                break;
+            }
+
+            case '4': {
+                repQty();
+                break;
+            }
+
+            case '5': {
+                repCost();
+                break;
+            }
+
+            case '6': {
+                repAge();
+                break;
+            }
+
+            case '7': {
+                break;
+            }
+
+            default :{
+                cout << "\x1B[2J\x1B[H";
+                cout << "Invalid entry, enter a value 1 -> 7, press enter to continue";
+                pause();
+                break;
+            }
+        }
+    }while( c != '7');
+}
+void repListing(){
+    cout << "\x1B[2J\x1B[H";
+    cout << "=== Inventory Listing ===\n";
+    cout << "[stub] Not implemented yet. Press Enter to continue.\n\n";
+    pause();
+}
+
+void repWholesale(){
+    cout << "\x1B[2J\x1B[H";
+    cout << "=== Inventory Wholesale Value ===\n";
+    cout << "[stub] Not implemented yet. Press Enter to continue.\n\n";
+    pause();
+}
+
+void repRetail(){
+    cout << "\x1B[2J\x1B[H";
+    cout << "=== Inventory Retail Value ===\n";
+    cout << "[stub] Not implemented yet. Press Enter to continue.\n\n";
+    pause();
+}
+
+void repQty(){
+    cout << "\x1B[2J\x1B[H";
+    cout << "=== Listing by Quantity ===\n";
+    cout << "[stub] Not implemented yet. Press Enter to continue.\n\n";
+    pause();
+}
+
+void repCost(){
+    cout << "\x1B[2J\x1B[H";
+    cout << "=== Listing by Cost ===\n";
+    cout << "[stub] Not implemented yet. Press Enter to continue.\n\n";
+    pause();
+}
+
+void repAge(){
+    cout << "\x1B[2J\x1B[H";
+    cout << "=== Listing by Age ===\n";
+    cout << "[stub] Not implemented yet. Press Enter to continue.\n\n";
+    pause();
+}
+
+void pause(){
+  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+  cin.get();
+}
